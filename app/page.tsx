e4644c569c5d11478a41d007c633db445a95d878
mr-afeef <mrafeef008@gmail.com>
2025-08-27 01:51:03 -0700

    error resolving

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Service from "@/components/Service";
 import Portfolio from "@/components/Portfolio";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
